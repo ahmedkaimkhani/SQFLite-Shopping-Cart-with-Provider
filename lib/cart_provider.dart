0d766ee511f5257db5a3e7ca105a46665e60df97
ahmedkaimkhani <ak9302359@gmail.com>
2024-03-13 17:05:13 +0500

    SQFLite Shopping Cart with Provider

--- lib/cart_provider.dart
+++ lib/cart_provider.dart
@@ -15,4 +15,23 @@ class CartProvider with ChangeNotifier {
     prefs.setDouble('total_price', _totalPrice);
     notifyListeners();
   }
+
+  void _getPrefItems() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _counter = prefs.getInt('cart_item') ?? 0;
+    _totalPrice = prefs.getDouble('total_price') ?? 0.0;
+    notifyListeners();
+  }
+
+  void addCounter() {
+    _counter++;
+    _setPrefItems();
+    ChangeNotifier();
+  }
+
+  void removeCounter() {
+    _counter--;
+    _setPrefItems();
+    ChangeNotifier();
+  }
 }
